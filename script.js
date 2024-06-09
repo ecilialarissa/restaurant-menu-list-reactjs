26fdca0d55d73819cab9d933824478f0b30ccdda
Ecilia Larissa <ecilialarissa@gmail.com>
2024-06-09 20:11:57 -0300

    funções javascript

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const menu = document.getElementById("menu")
 const cartBtn = document.getElementById("cart-btn")
 const cartModal = document.getElementById("cart-modal")
-const cartItemContainer = document.getElementById("cart-items")
+const cartItemsContainer = document.getElementById("cart-items")
 const cartTotal = document.getElementById("cart-total")
 const checkoutBtn = document.getElementById("checkout-btn")
 const closeModalBtn = document.getElementById("close-modal-btn")
@@ -36,8 +36,8 @@ menu.addEventListener("click", function(event){
         const name = parentButton.getAttribute("data-name")
         const price = parseFloat(parentButton.getAttribute("data-price"))
 
-        console.log(name)
-        console.log(price)
+       // console.log(name)
+        //console.log(price)
 
         addToCart(name, price)
     }
@@ -50,14 +50,112 @@ function addToCart(name,price){
 
     if(existingItem){
         existingItem.quantity += 1;
-        return;
+        
+    }else{
+        cart.push({
+        name,
+        price,
+        quantity: 1,
+    })
+
+    
     }
 
-cart.push({
-    name,
-    price,
-    quantity: 1,
-})
+    updateCartModal()
+
 
 } 
+
+function updateCartModal() {
+    cartItemsContainer.innerHTML = "";
+    let total = 0; 
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div");
+        cartItemElement.classList.add("justify-between", "mb-4", "flex-col")
+
+
+        cartItemElement.innerHTML = `
+        <div class = "flex items-center justify-between" >
+            <div>
+                <p class=" font-semibold " >${item.name}</p>
+                <p> Qtd: ${item.quantity}</p>
+                <p class="font-bold mt-2">R$ ${item.price.toFixed(2)}</p>
+            </div>
+                
+                <button class="remove-from-cart-btn" data-name="${item.name}">
+                Remover
+                </button>
+               
+
+            </div>
+        `
+
+        total += item.price * item.quantity;
+
+        cartItemsContainer.appendChild(cartItemElement)    
+    })
+        cartTotal.textContent = total.toLocaleString("pt-BR", {
+            style: "currency",
+            currency:"BRL"
+        });
+    
+    cardCounter.innerHTML = cart.length; 
+
+}
+
+// função remover item do carrinho
+
+cartItemsContainer.addEventListener("click", function (event){
+    if(event.target.classList.contains("remove-from-cart-btn")){
+        const name = event.target.getAttribute("data-name")
+        
+    
+      removeItemCart(name);
+    }
+
+})
+
+function removeItemCart(name){
+    const index = cart.findIndex(item => item.name === name);
+    if(index !==  -1){
+        const item = cart[index];
+
+        if (item.quantity > 1){
+            item.quantity -= 1;
+            updateCartModal();
+            return;
+        }
+
+        //utilizando o método .splice para retirar items de contagens maiores que 1
+        cart.splice(index, 1); 
+        updateCartModal();
+
+
+    }
+}
+
+// input endereço 
+    checkoutBtn.addEventListener("input", function(event){
+        let inputValue = event.target.value;
+
+        // verificação do campo endereço 
+
+
+    })
+
+
+//finalizar pedido 
+
+checkoutBtn.addEventListener("click", function(){
+    if (cart.length === 0)
+        return;
+    if(addressInput.value === ""){
+        addressWarn.classList.remove("hidden")
+        addressInput.classList.add("border-red-500")
+        
+    }
+    
+})
+    
 
