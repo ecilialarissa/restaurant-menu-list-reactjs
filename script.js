9cca5bcbabacf351e89fca61d6760b5127664c60
Ecilia Larissa <ecilialarissa@gmail.com>
2024-06-12 22:22:58 -0300

    alt - ajuste horario

--- script.js
+++ script.js
@@ -183,7 +183,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestaurantOpen(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 18 && hora < 22;
+    return hora >= 18 && hora < 23;
 
 }
 
